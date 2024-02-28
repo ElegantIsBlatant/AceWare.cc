b0e7281902feac791390978571f7001e027670c2
ElegantIsBlatant <161535962+ElegantIsBlatant@users.noreply.github.com>
2024-02-27 20:23:24 -0500

    Update silent-and-smoothlock.lua

--- silent-and-smoothlock.lua
+++ silent-and-smoothlock.lua
@@ -15,4 +15,8 @@ getgenv().Enabled = true
 -- // main script use with silent aim / / -- 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ElegantIsBlatant/AceWare.cc/main/silentaim.lua"))()
+
+
+-- // perm c tool / / --
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ElegantIsBlatant/AceWare.cc/main/permtool.lua"))()
